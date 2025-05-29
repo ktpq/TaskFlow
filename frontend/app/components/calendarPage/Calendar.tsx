7ae245bc25e0144b6c6eea1f32ebb0c3a236d4cb
Kartpol123 <kartpol47@gmail.com>
2025-05-29 20:25:59 +0700

    update

--- frontend/app/components/calendarPage/Calendar.tsx
+++ frontend/app/components/calendarPage/Calendar.tsx
@@ -1,29 +1,23 @@
-import React from 'react'
+"use client";
+import React from "react";
 
-
-
-import MyCalendar from './MyCalendar'
-import "aos/dist/aos.css"
-import AOS from "aos"
-import { useEffect } from 'react'
-
-
+import MyCalendar from "./MyCalendar";
+import "aos/dist/aos.css";
+import AOS from "aos";
+import { useEffect, useState } from "react";
 
 export default function Calendar() {
-  useEffect(() =>{
-    AOS.init()
-  })
-    
+  useEffect(() => {
+    AOS.init();
+  });
+
   return (
     <main className="grow px-7 py-7 ml-[290px]">
-        {/* <button className="px-2 rounded-lg hover:bg-gray-300 duration-200 border"> Back </button> */}
-        <div data-aos="fade-down" data-aos-duration="1000">
-            <h1 className="text-3xl mt-5 mb-3 font-extrabold"> Team calendar </h1>
-            <p> View and manage your team's schedule </p>
-            <MyCalendar/>
-        </div>
-        
-        
+      {/* <button className="px-2 rounded-lg hover:bg-gray-300 duration-200 border"> Back </button> */}
+      <div data-aos="fade-down" data-aos-duration="1000">
+        <h1 className="text-3xl mt-5 mb-3 font-extrabold"> Team calendar </h1>
+        <p> View and manage your team's schedule </p>
+      </div>
     </main>
-  )
+  );
 }
